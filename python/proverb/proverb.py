efb0dfdd872c73381ab446e553e49c3d71d435f9
Joseph Clark <joseph@jclark.codes>
2024-01-22 18:10:45 -0500

    finished proverb exercise

--- python/proverb/proverb.py
+++ python/proverb/proverb.py
@@ -2,6 +2,9 @@ def proverb(*input_data, **qualifier):
     result = [f"For want of a {noun1} the {noun2} was lost." for noun1, noun2 in zip(input_data, input_data[1:])]
     
     if len(input_data) > 0:
-        result.append(f"And all for the want of a {qualifier['qualifier']} {input_data[0]}.")
+        if not qualifier['qualifier']:
+            result.append(f"And all for the want of a {input_data[0]}.")
+        else:
+            result.append(f"And all for the want of a {qualifier['qualifier']} {input_data[0]}.")
     
     return result
